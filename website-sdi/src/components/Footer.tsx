a4bbef37b93722b7be6884d1e0f1d37fe8296de6
Luiz Arlochi <luiz.arlochi@gmail.com>
2024-09-10 13:48:41 -0300

    add font

--- website-sdi/src/components/Footer.tsx
+++ website-sdi/src/components/Footer.tsx
@@ -10,6 +10,11 @@ const Footer: React.FC = () => {
           alt="Price Box"
           className=" w-[270px] h-auto mb-5"
         />
+        <button className="mt-4 mb-10 px-10 py-7 bg-red-600 text-white text-xl font-semibold rounded-[50px] shadow-md hover:bg-red-700 transition duration-300">
+        <a href="https://pay.kiwify.com.br/C0U7dd6">
+        EU QUERO MEU GUIA!
+        </a>
+      </button>
       </div>
             <div className="bg-gradient-green text-white text-center h-28 flex items-center justify-center">
       <p className="text-white text-4xl md:text-5xl font-bold">
