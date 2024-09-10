cbb59373ad363045947aed5816175fbdb6c2e494
Luiz Arlochi <luiz.arlochi@gmail.com>
2024-09-10 14:07:31 -0300

    font fix

--- website-sdi/src/components/Feedback.tsx
+++ website-sdi/src/components/Feedback.tsx
@@ -6,11 +6,11 @@ const Feedback: React.FC = () => {
   return (
     <div>
       <div className="bg-gradient-green text-white text-center h-28 flex items-center justify-center">
-        <p className="text-4xl md:text-5xl font-bold">FEEDBACK DOS ALUNOS:</p>
+        <p className="text-4xl md:text-5xl font-bold no-paragraph-font">FEEDBACK DOS ALUNOS:</p>
       </div>
-    <div className="py-8 flex justify-center">
-      <img className="rounded-[55px] px-3" src={FeedbackImg} alt="Feedback" />
-    </div>
+      <div className="py-8 flex justify-center">
+        <img className="rounded-[55px] px-3" src={FeedbackImg} alt="Feedback" />
+      </div>
     </div>
   );
 }
